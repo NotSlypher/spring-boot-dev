41bea561ddf891fd49b6762b507ec51d394d421b
Ayush Gupta <ayushgupta6719@gmail.com>
2024-01-09 09:28:50 +0530

    actuator

---
+++ 01-spring-boot-overview/04-actuator-security-demo/src/main/java/com/slypher/springboot/myhotapp/MyhotappApplication.java
@@ -0,0 +1,13 @@
+package com.slypher.springboot.myhotapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyhotappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyhotappApplication.class, args);
+	}
+
+}
---
+++ 01-spring-boot-overview/04-actuator-security-demo/src/main/java/com/slypher/springboot/myhotapp/rest/FunRestController.java
@@ -0,0 +1,18 @@
+package com.slypher.springboot.myhotapp.rest;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+    //expose "/" that returns "Hello World"
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello World";
+    }
+
+    @GetMapping("/review")
+    public String showReview() { return "yo bro!!!!"; }
+}
